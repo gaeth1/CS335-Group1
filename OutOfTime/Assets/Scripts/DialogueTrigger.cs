685b16f2edbf5d2ffd686568dc1ecf4b855572fd
Gaeth <galalou@knox.edu>
2020-05-12 23:00:08 -0700

    DialogueScreen

---
+++ OutOfTime/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Dialogue dialogue;
+
+    public void TriggerDialogue(){
+        FindObjectOfType<DialogueManger>().StartDialogue(dialogue);
+    }
+
+}
