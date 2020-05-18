f74daf0e13e7437c5f7b750921075ea8073fcb72
Gaeth <galalou@knox.edu>
2020-05-18 16:14:42 -0700

    audio through scene 3. Animations for party scene

---
+++ OutOfTime/Assets/Scripts/PartyDialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+        StopAllCoroutines();
+        StartCoroutine(wait());       
+    }
+
+    IEnumerator wait(){
+        yield return new WaitForSeconds(11);
+        animator.SetBool("isOpen", true);
+
+    }
+}
